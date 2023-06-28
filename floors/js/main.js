c85dbc047bec26b75d7bba3ed5f174d89e9fe092
Rod101 <rodney.addo@gmail.com>
2023-06-28 09:16:51 +0200

    moving files and creating projects

---
+++ floors/js/main.js
@@ -0,0 +1,3 @@
+// Add the paths to your slider images in the 'slideImages' array
+const slideImages = ['../images/slider/floors-header-slider-desktop-1.jpg', '../images/slider/floors-header-slider-desktop-1.jpg', '../images/slider/floors-header-slider-desktop-1.jpg'];
+let currentSlideIndex
